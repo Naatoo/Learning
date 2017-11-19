cc534e979418d8bd32d2b85d6a43599e2b3a8a49
natooo <thenatooorat@gmail.com>
2017-11-19 01:37:16 +0100

    Reorganize dir

---
+++ MySQL/Shop/shop.sql
@@ -0,0 +1,18 @@
+﻿DROP DATABASE IF EXISTS shop;
+CREATE DATABASE shop;
+USE shop;
+
+CREATE TABLE products(
+id int NOT NULL AUTO_INCREMENT,
+name CHAR(20),
+price INT,
+PRIMARY KEY (id)
+);
+
+SHOW TABLES;
+INSERT INTO products (name, price) VALUES
+("milk", 5),
+("juice", 4),
+("butter", 12);
+
+SELECT * FROM products;
