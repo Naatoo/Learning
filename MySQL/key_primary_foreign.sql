ee65b831f80d82445eab7284c4433d6365acf721
natooo <thenatooorat@gmail.com>
2017-11-22 23:55:43 +0100

    Primary, foreign key

---
+++ MySQL/key_primary_foreign.sql
@@ -0,0 +1,27 @@
+﻿DROP DATABASE IF EXISTS buildings;
+CREATE DATABASE buildings;
+USE buildings;
+
+CREATE TABLE house(
+	number INT UNSIGNED,
+	name char(30),
+	surface FLOAT);
+
+INSERT INTO house
+VALUES  (1, 'apartment', 70),
+	(2, 'of', 150),
+	(3, 'df', 270),
+	(4, 'block', 3000);
+
+ALTER TABLE house 
+	ADD PRIMARY KEY AUTO_INCREMENT(number),
+	ADD CONSTRAINT uq_house UNIQUE(name);
+
+CREATE TABLE street(
+	number INT UNSIGNED,
+	name char(20));
+
+ALTER TABLE street
+	ADD PRIMARY KEY AUTO_INCREMENT(number), 
+	ADD CONSTRAINT uq_house UNIQUE(name),
+	ADD FOREIGN KEY(number) REFERENCES buildings.house(number);
